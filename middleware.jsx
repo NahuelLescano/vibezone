426fa40185c115a2142f2d918ba63ca7a7c3679a
FacundoAylan <facundogsb@gmail.com>
2024-05-11 14:34:14 -0300

    subiendo cambios nuevos

--- middleware.jsx
+++ middleware.jsx
@@ -1,6 +1,9 @@
+import { AuthMiddleware } from "@clerk/nextjs";
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  publicRouters: ['/api/webhook']
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
