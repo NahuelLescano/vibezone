64db28193fd8d02d61c732c39c328ba92a3a1f5f
FacundoAylan <facundogsb@gmail.com>
2024-05-13 17:40:37 -0300

    subiendo arreglos

--- app/api/webhook/route.jsx
+++ app/api/webhook/route.jsx
@@ -50,45 +50,45 @@ export async function POST(req) {
 
   // Handle the event
   const eventType = evt?.type;
-  console.log(eventType)
-  // if (eventType === "user.created" || eventType === "user.updated") {
-  //   const { id, first_name, last_name, image_url, email_addresses, username } =
-  //     evt?.data;
 
-  //   try {
-  //     await createOrUpdateUser(
-  //       id,
-  //       first_name,
-  //       last_name,
-  //       image_url,
-  //       email_addresses,
-  //       username
-  //     );
+  if (eventType === "user.created" || eventType === "user.updated") {
+    const { id, first_name, last_name, image_url, email_addresses, username } =
+      evt?.data;
 
-  //     return new Response("User is created or updated", {
-  //       status: 200,
-  //     });
-  //   } catch (err) {
-  //     console.error("Error creating or updating user:", err);
-  //     return new Response("Error occured", {
-  //       status: 500,
-  //     });
-  //   }
-  // }
+    try {
+      await createOrUpdateUser(
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses,
+        username
+      );
 
-  // if (eventType === "user.deleted") {
-  //   try {
-  //     const { id } = evt?.data;
-  //     await deleteUser(id);
+      return new Response("User is created or updated", {
+        status: 200,
+      });
+    } catch (err) {
+      console.error("Error creating or updating user:", err);
+      return new Response("Error occured", {
+        status: 500,
+      });
+    }
+  }
 
-  //     return new Response("User is deleted", {
-  //       status: 200,
-  //     });
-  //   } catch (err) {
-  //     console.error("Error deleting user:", err);
-  //     return new Response("Error occured", {
-  //       status: 500,
-  //     });
-  //   }
-  // }
+  if (eventType === "user.deleted") {
+    try {
+      const { id } = evt?.data;
+      await deleteUser(id);
+
+      return new Response("User is deleted", {
+        status: 200,
+      });
+    } catch (err) {
+      console.error("Error deleting user:", err);
+      return new Response("Error occured", {
+        status: 500,
+      });
+    }
+  }
 }
