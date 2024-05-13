adc4f2c270fb689198769a4a070cbbfd5e570208
FacundoAylan <facundogsb@gmail.com>
2024-05-13 20:33:32 -0300

    subiendo arreglos

--- app/api/webhooks/route.jsx
+++ app/api/webhooks/route.jsx
@@ -52,7 +52,7 @@ export async function POST(req) {
   if(eventType === 'user.created'|| eventType === "user.updated"){
     const { id, first_name, last_name,image_url,email_addresses, username } = evt.data;
     try {
-      createOrUpdateUser(user);
+      createOrUpdateUser(id,first_name,last_name,image_url,email_addresses,username);
       return new Response("User is created or updated", {
         status: 200,
       });
