cccfbcd9a9fd03d71f8db05fdc6628ae789ff5e3
FacundoAylan <facundogsb@gmail.com>
2024-05-13 17:16:11 -0300

    subiendo arreglos

--- lib/actions/user.jsx
+++ lib/actions/user.jsx
@@ -1,34 +1,43 @@
 import User from "@lib/models/User";
 import { connectToDB } from "@lib/mongodb/mongoose";
 
-export const createUser = async({id, first_name, last_name, image_url, email_addresses, username})=>{
-  try{
+export const createOrUpdateUser = async (
+  id,
+  first_name,
+  last_name,
+  image_url,
+  email_addresses,
+  username
+) => {
+  try {
     await connectToDB();
+
     const user = await User.findOneAndUpdate(
-      {clerkId: id},
+      { clerkId: id },
       {
-        $set:{
+        $set: {
           firstName: first_name,
           lastName: last_name,
           profilePhoto: image_url,
-          email: email_addresses[0].email_addresses,
+          email: email_addresses[0].email_address,
           username: username,
-        }
+        },
       },
-      {upsert:true,new: true}
+      { upsert: true, new: true } 
     );
-    await user.save()
-    return user
-  }catch(error){
-    console.log(error)
-  }
-}
 
-export const deleteUser = async (id)=>{
-  try{
-    await connectToDB();
-    await User.findOneAndDelete({clerkId: id})
-  }catch(error){
-    console.log(error)
+    await user.save();
+    return user;
+  } catch (error) {
+    console.error(error);
   }
-}
+};
+
+export const deleteUser = async (id) => {
+  try {
+    await connectToDB();
+    await User.findOneAndDelete({ clerkId: id });
+  } catch (error) {
+    console.error(error);
+  }
+};
