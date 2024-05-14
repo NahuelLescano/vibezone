a0ecff9489e014de040dbad01495b1d17eb8edb6
FacundoAylan <facundogsb@gmail.com>
2024-05-13 21:37:01 -0300

    subiendo arreglos

--- app/api/webhooks/route.jsx
+++ app/api/webhooks/route.jsx
@@ -33,7 +33,7 @@ async function handleUserEvent(userData) {
     "username": "fakugsb",
   }
   const { id, last_name, image_url, email_addresses, username } = userData1;
-  await createOrUpdateUser(id, first_name, last_name, image_url, email_addresses, username);
+  await createOrUpdateUser(id, last_name, image_url, email_addresses, username);
 }
 
 export async function POST(req) {
